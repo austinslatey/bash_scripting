5a70d76f81a92d8bbe481af4430c6d2ac49f8b2a
Austin Slater <austinslater58@gmail.com>
2023-09-25 15:46:45 -0500

    README.md updated script now uses input validation, arrays, and a function

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# bash_scripting
+# CLI Greeting Script
 
-### Notes
-Run any script in terminal (bash) by typing `./script.sh`
+## Description:
+This Bash script allows you to greet the user with customizable messages. You can specify the number of greetings and customize the greetings in the script.
+
+## Usage:
+- Run the script without any command-line arguments:
+  ```bash
+  ./greeting_script.sh
+
+## Instructions:
+- When you run the script, it will prompt you with the question: "How many times should I greet?"
+
+- Enter a positive integer to specify how many times you want to be greeted.
+
+- The script will then greet you the specified number of times with a cycle of greetings.
+
+- The greetings are customizable. You can modify the greetings array in the script to change the messages
+
+## Example 
+`bash`
+```
+$ ./greeting_script.sh
+How many times should I greet?
+4
+Nice to meet you!
+How are you?
+Hello there!
+Nice to meet you!
+```
--- conditionals.sh
+++ conditionals.sh
@@ -1,15 +1,36 @@
-first_greeting="Nice to meet you!"
-later_greeting="How are you?"
-greeting_occasion=0
+#!/bin/bash
+
+# Check if there are any command-line arguments
+if [ $# -gt 0 ]; then
+  echo "Usage: $0"
+  exit 1
+fi
+
+# Function to validate if input is a positive integer
+validate_input() {
+  if [[ ! $1 =~ ^[1-9][0-9]*$ ]]; then
+    echo "Please enter a valid positive integer."
+    exit 1
+  fi
+}
+
+# Array of greetings
+greetings=("Nice to meet you!" "How are you?" "Hello there!")
+
 echo "How many times should I greet?"
 read greeting_limit
-while [ $greeting_occasion -lt $greeting_limit ]
+
+# Validate the input using the validate_input function
+validate_input "$greeting_limit"
+
+# Rest of your script
+greeting_occasion=1  # Initialize to 1 to allow the initial greeting
+
+while [ $greeting_occasion -le $greeting_limit ]
 do
-  if [ $greeting_occasion -lt 1 ]
-  then
-    echo $first_greeting
-  else
-    echo $later_greeting
-  fi
+  # Calculate the index for cycling through greetings
+  greeting_index=$((greeting_occasion % ${#greetings[@]}))
+  echo "${greetings[$greeting_index]}"
   greeting_occasion=$((greeting_occasion + 1))
 done
+
